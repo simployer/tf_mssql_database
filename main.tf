a3be54b4b22bc790fffdecb54fec37c82cf4b6e5
martinhelgesen <martin.helgesen@simployer.com>
2022-12-20 12:29:00 +0100

    added retention policies

--- main.tf
+++ main.tf
@@ -4,4 +4,10 @@ resource "azurerm_mssql_database" "this" {
   collation = "SQL_Latin1_General_CP1_CI_AS"
   tags = var.tags
   elastic_pool_id = var.elastic_pool_id
+  short_term_retention_policy {
+    retention_days = 7
+  }
+  long_term_retention_policy {
+    weekly_retention = "P25W"
+  }
 }
